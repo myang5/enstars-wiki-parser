fdb95d3b83bc52893b0474a4a3b47695f3c8516a
Midori Yang <myang5@wellesley.edu>
2020-10-03 13:19:22 -0400

    eslint error correction

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,26 +2,60 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default function Header() {
+  const lastUpdated = '9/20/20';
+  const desc = (
+    <>
+      A website to more easily upload event/gacha stories from the mobile idol game Ensemble Stars
+      to the fandom wiki.
+      <br />
+      It formats your story chapter into text that can be pasted directly into the
+      &quot;source&quot; section of the page.
+      <br />
+      Developed by
+      {' '}
+      <a target="_blank" rel="noreferrer" href="https://twitter.com/gayandasleep">
+        midori
+      </a>
+      {` (last updated ${lastUpdated}).`}
+    </>
+  );
+
   return (
     <header>
       <h1>
-        <Link to='/'>ENSTARS STORY FORMATTER</Link>
+        <Link to="/">ENSTARS STORY FORMATTER</Link>
       </h1>
       <div className="horizontal">
-        <p>
-          A website to more easily upload event/gacha stories from the mobile idol game Ensemble Stars to the fandom wiki.
-          <br />It takes formats your story chapter into text that can be pasted directly into the "source"
-        section of the page.
-          <br />Developed by <a target="_blank" href="https://twitter.com/gayandasleep">midori</a> (last updated 9/20/20).
-        </p>
+        <p>{desc}</p>
         <ul id="navbar">
-          <li><Link to='/howto'>HOW TO USE</Link></li>
-          <li><a target="_blank" href="https://github.com/myang5/enstars-wiki-parser/issues">KNOWN ISSUES</a></li>
-          <li><a target="_blank" href="https://twitter.com/gayandasleep">CONTACT</a></li>
-          <li><a target="_blank" href="https://github.com/myang5/enstars-wiki-parser">GITHUB</a></li>
+          <li>
+            <Link to="/howto">HOW TO USE</Link>
+          </li>
+          <li>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://github.com/myang5/enstars-wiki-parser/issues"
+            >
+              KNOWN ISSUES
+            </a>
+          </li>
+          <li>
+            <a target="_blank" rel="noreferrer" href="https://twitter.com/gayandasleep">
+              CONTACT
+            </a>
+          </li>
+          <li>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://github.com/myang5/enstars-wiki-parser"
+            >
+              GITHUB
+            </a>
+          </li>
         </ul>
       </div>
     </header>
-  )
+  );
 }
-
